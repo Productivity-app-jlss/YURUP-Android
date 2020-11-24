10e1670b79418da01635602eab94a97c436f4dd4
lishannon <lishannon490@gmail.com>
2020-11-24 01:35:48 -0500

    implemented the floating button and added activity screen

--- app/src/main/java/com/android/yurup/MainActivity.java
+++ app/src/main/java/com/android/yurup/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.yurup;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,8 +13,13 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewAnimationUtils;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab_main, fab_create, fab_join;
+    boolean isRotate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +28,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab_main = findViewById(R.id.fab_main);
+        final FloatingActionButton fab_create = findViewById(R.id.fab_create);
+        final FloatingActionButton fab_join = findViewById(R.id.fab_join);
+
+        Animation.init(fab_create);
+        Animation.init(fab_join);
+
+        fab_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(MainActivity.this, "Add challenge", Toast.LENGTH_SHORT).show();
+                isRotate = Animation.rotateFab(view, !isRotate);
+                if(isRotate){
+                    Animation.showIn(fab_create);
+                    Animation.showIn(fab_join);
+                }else{
+                    Animation.showOut(fab_create);
+                    Animation.showOut(fab_join);
+                }
+
             }
         });
+
+        fab_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Create challenge", Toast.LENGTH_SHORT).show();
+                Intent create_activity = new Intent(getApplicationContext(), createActivity.class);
+                startActivity(create_activity);
+            }
+        });
+
+        fab_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Join challenge", Toast.LENGTH_SHORT).show();
+                Intent join_activity = new Intent(getApplicationContext(), joinActivity.class);
+                startActivity(join_activity);
+            }
+        });
+
     }
 
     @Override
